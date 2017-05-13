7eb5054a842408559c7ab3428cec4bed31ac5e86
mfaishalakbar <faisalwin7@gmail.com>
2017-05-13 22:12:17 +0700

    Initial Push on Github.

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl216/tmdbdroid/AppVariable.java
@@ -0,0 +1,9 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl216.tmdbdroid;
+
+/**
+ * Created by nerdywoffy on 5/13/17.
+ */
+
+public class AppVariable {
+    public static String TMDB_APIKEY = "04cb09ef9225cd14e21a36c226cd084c";
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl216/tmdbdroid/MainActivity.java
@@ -0,0 +1,123 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl216.tmdbdroid;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.SearchView;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        final MenuItem myActionMenuItem = menu.findItem( R.id.search);
+        final SearchView searchView = (SearchView) myActionMenuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if( ! searchView.isIconified()) {
+                    searchView.setIconified(true);
+                }
+                myActionMenuItem.collapseActionView();
+                Toast.makeText(getApplicationContext(),query,Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String s) {
+                // UserFeedback.show( "SearchOnQueryTextChanged: " + s);
+                return false;
+            }
+
+        });
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        String fragmentTitle = "";
+
+        // Movies
+        if(id == R.id.mov_popular){
+            fragmentTitle = "Popular";
+        }else if(id == R.id.mov_now){
+            fragmentTitle = "Now Playing";
+        }else if(id == R.id.mov_upcoming){
+            fragmentTitle = "Upcoming";
+        }
+
+        // TV Shows
+        if(id == R.id.tv_popular){
+            fragmentTitle = "Popular";
+        }else if(id == R.id.tv_now){
+            fragmentTitle = "Now Airing";
+        }else if(id == R.id.tv_upcoming){
+            fragmentTitle = "Upcoming";
+        }
+
+        if(id == R.id.nav_saved){
+            fragmentTitle = "Saved";
+        }
+
+        setTitle(fragmentTitle);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
